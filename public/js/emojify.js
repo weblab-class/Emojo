2fafe179514565d2dc43505b7c24d9f89a384296
Meryl Wang <wangmeryl77@gmail.com>
2019-01-22 01:50:07 -0500

    pulling

--- public/js/emojify.js
+++ public/js/emojify.js
@@ -1,16 +1,27 @@
 // takes plaintext and adds emoji / replaces text with emoji
 // import emojify.html, fuzz package
+
 if (user._id !== undefined) {
 	document.getElementById('log').innerText="LOGOUT😘"
 }
 
+console.log(fuzzball.ratio("fuzz", "fuzzy"));
+//TODO: get user input from emojify.html
+// id="emojify-input"
 
-// get user input from emojify.html
-// use fuzz to match post words to database keywords
-// get toggle state. replace / append emojis 
+// TODO: access emoji data
+// TODO: use fuzz to match post words to database keywords
 
-// worry about data structure later
-// FIXME: store post (copy from catbook)
+// TODO: get toggle state. replace / append emojis 
+// id="button-16"
+
+// TODO:trigger on button click
+// id="emojifyBtn"
+
+//TODO: Post. worry about data structure later
+// id="postBtn"
+// store post (copy from catbook)
     // assign ID to post
+    // add post to db
     // add ID to user.posts
     // post: content, author, timestamp
