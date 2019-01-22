ebed2847f3a3909ae9ffea2c96f7c0155dab86af
Meryl Wang <wangmeryl77@gmail.com>
2019-01-22 00:03:34 -0500

    working on emojifier

--- emojify.js
+++ emojify.js
@@ -1,4 +1,9 @@
+// import emojify.html, fuzz package
+
 // get user input from emojify.html
 // use fuzz to match post words to database keywords
-// replace / append emojis to post
-// store post in mongo
+// get toggle state. replace / append emojis 
+// FIXME: store post (copy from catbook)
+    // assign ID to post
+    // add ID to user.posts
+    // post: content, author, timestamp
