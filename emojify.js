8e5ab1131ba9684c728cf2f6c7a7fa5bb2332fb4
Meryl Wang <wangmeryl77@gmail.com>
2019-01-22 00:34:06 -0500

    pull request

--- emojify.js
+++ emojify.js
@@ -1,8 +1,11 @@
+// takes plaintext and adds emoji / replaces text with emoji
 // import emojify.html, fuzz package
 
 // get user input from emojify.html
 // use fuzz to match post words to database keywords
 // get toggle state. replace / append emojis 
+
+// worry about data structure later
 // FIXME: store post (copy from catbook)
     // assign ID to post
     // add ID to user.posts
