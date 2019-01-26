c2f5dafea873a5459cab4a3abef17de362b03128
unknown <vercinge@mit.edu>
2019-01-25 22:25:07 -0500

    get ready for merge drama!

--- src/routes/api.js
+++ src/routes/api.js
@@ -4,7 +4,7 @@ const connect = require('connect-ensure-login');
 
 // models
 const User = require('../models/user');
-// const Story = require('../models/story');
+const Story = require('../models/story');
 // const Comment = require('../models/comment');
 
 const router = express.Router();
@@ -25,35 +25,34 @@ router.get('/user', function(req, res) {
   });
 });
 
-// router.get('/stories', function(req, res) {
-//   Story.find({}, function(err, stories) {
-//     res.send(stories);
-//   });
-// });
+router.get('/stories', function(req, res) {
+  Story.find({}, function(err, stories) {
+    res.send(stories);
+  });
+});
 
-// router.post(
-//   '/story',
-//   connect.ensureLoggedIn(),
-//   function(req, res) {
-//     User.findOne({ _id: req.user._id },function(err,user) {
-//       const newStory = new Story({
-//         'creator_id': user._id,
-//         'creator_name': user.name,
-//         'content': req.body.content,
-//       });
-
-//       user.set({ last_post: req.body.content });
-//       user.save(); // this is OK, because the following lines of code are not reliant on the state of user, so we don't have to shove them in a callback. 
-
-//       newStory.save(function(err,story) {
-//         // configure socketio
-//         if (err) console.log(err);
-//       });
-
-//       res.send({});
-//     });
-//   }
-// );
+router.post(
+  '/story',
+  connect.ensureLoggedIn(),
+  function(req, res) {
+    User.findOne({ _id: req.user._id },function(err,user) {
+      const newStory = new Story({
+        'userid': user._id,
+        'username': user.name,
+        'content': req.body.content,
+        'timestamp': req.body.timestamp,
+        'tags': req.body.tags,
+      });
+      //user.set({ last_post: req.body.content });
+      user.save(); // this is OK, because the following lines of code are not reliant on the state of user, so we don't have to shove them in a callback. 
+      newStory.save(function(err,story) {
+        // configure socketio
+        if (err) console.log(err);
+      });
+      res.send({});
+    });
+  }
+);
 
 // router.get('/comment', function(req, res) {
 //   Comment.find({ parent: req.query.parent }, function(err, comments) {
