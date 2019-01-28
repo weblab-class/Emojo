67da49579ccb54753c6a2d4a6f6725fd6d8f26d1
unknown <vercinge@mit.edu>
2019-01-27 19:28:10 -0500

    added smart post button

--- public/js/emojify.js
+++ public/js/emojify.js
@@ -14,10 +14,14 @@ function updateButton(user) {
 	if (user._id !== undefined) {
 		document.getElementById('log').innerText="LOGOUT😘";
 		document.getElementById('log').href="/logout";
+		document.getElementById('postBtn').class="btn btn-primary btn-lg btn-block";
+		document.getElementById('postBtn').innerText="POST📌";
 	}
 	else {
 		document.getElementById('log').innerText="LOGIN📂";
 		document.getElementById('log').href="./auth/google";
+		document.getElementById('postBtn').class="btn btn-primary btn-lg btn-block disabled";
+		document.getElementById('postBtn').innerText="POST📌 (LOG IN FIRST)";
 	}
 }
 
